7c376de54fa521025a6b3cf228a117f068d68ac8
vitor <vitor.grosskopf@gmail.com>
2024-11-03 21:12:08 -0300

    Adds rails Session cookies

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,6 +6,7 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path, notice: "Successfuly created an account! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧	"
     else
       render :new
