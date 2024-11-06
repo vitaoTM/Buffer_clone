16be220773c104cf88f190b0313e01f1124bca79
vitor <vitor.grosskopf@gmail.com>
2024-11-05 23:07:43 -0300

    Add loggout button to view - Update routes: sign in and sign out - Add destroy action to controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'about', to: 'about#index'
 
   get 'sign_up', to: 'registrations#new'
-  # post 'users', to: 'registrations#create'
+  post 'sign_up', to: 'registrations#create'
+  delete 'loggout', to: 'sessions#destroy'
   root to: 'main#index'
 end
