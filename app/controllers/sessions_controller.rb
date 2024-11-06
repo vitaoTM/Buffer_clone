ad0d0df61cd02fa98077e4e5a238d7a5b47bfc25
vitor <vitor.grosskopf@gmail.com>
2024-11-06 15:22:10 -0300

    Add routes: new and create to Sessions - Update controller actions: new & create - Controller: Add flash in verification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+      flash[:notice] = "Logged in Successfuly! (/￣ー￣)/~~☆’.･.･:★’.･.･:☆"
+    else
+      flash[:alert] = 'Invalid login (－‸ლ)	'
+      render :new
+    end
+  end
+
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "Logged out ლ(ಠ_ಠ ლ)"
+    redirect_to root_path
+    flash[:notice] =  "Logged out ლ(ಠ_ಠ ლ)"
   end
 end
