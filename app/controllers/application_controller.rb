05e6b097014215afbe6579bacd2b4568080c29e5
vitor <vitor.grosskopf@gmail.com>
2024-11-10 19:16:50 -0300

    Update controller - Fix typo sessions -> session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+
+  def set_current_user
+    if session[:user_id]
+      Current.user = User.find_by(id: session[:user_id])
+    end
+  end
+
+  def require_user_loged_in!
+    if Current.user.nil?
+      redirect_to sign_in_path
+      flash[:alert] = "You must Sign In to do that!"
+    end
+  end
+
 end
