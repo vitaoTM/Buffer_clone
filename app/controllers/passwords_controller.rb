c1fcc167dd65e469f550d7fe9029151c50a69dce
vitor <vitor.grosskopf@gmail.com>
2024-11-10 19:40:34 -0300

    Update controller - Upadate action: update - Add private method for permited params

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -4,9 +4,19 @@ class PasswordsController < ApplicationController
   def edit
 
   end
-  def update
 
+  def update
+    if Current.user.update(password_params)
+      redirect_to root_path, notice: "Password Updated!"
+    else
+      render :edit
+    end
   end
 
+  private
+
+  def password_params
+    params.require(:user).permit(:password, password_confirmation)
+  end
 
 end
