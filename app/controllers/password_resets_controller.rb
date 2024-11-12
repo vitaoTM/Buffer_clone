90327ef2ba4fc4fbd2e0c68a5752521ee2550d49
vitor <vitor.grosskopf@gmail.com>
2024-11-12 16:58:54 -0300

    Add actions: edit and update

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,8 +7,32 @@ class PasswordResetsController < ApplicationController
 
     if @user.present?
       #send email
-      PasswordMailer.with(user: @user).reset.deliver_later
+      PasswordMailer.with(user: @user).reset.deliver_now
     end
     redirect_to root_path, notice: "If an account with that email was found, an email was sent for recovery"
   end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "Token has expierd, please try again"
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Your password was reseted. Please Sign In"
+    else
+      render :edit
+    end
+
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
 end
