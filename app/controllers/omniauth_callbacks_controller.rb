58bad91ff33daafe3bda8131bc209f4f9e466741
vitor <vitor.grosskopf@gmail.com>
2024-11-13 22:47:32 -0300

    Update Omniauth controller - Update twitter - Update twitter action: 'grab infos and credentials from current user'

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,17 @@
 class OmniauthCallbacksController < ApplicationController
   def twitter
-    render plain: "SuCCESS?"
+    Rails.logger.info auth
+    Current.user.twitter_accounts.create(
+      name: auth.info.name,
+      username: auth.info.nickname,
+      image: auth.info.image,
+      token: auth.credentials.token,
+      secret: auth.credentials.secret,
+    )
   end
+
+  def auth
+    request.env['omniauth.auth']
+  end
+
 end
